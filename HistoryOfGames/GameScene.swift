093649e00f388f07aca38bb19bd54206da29ba1e
Pedro Velmovitsky <pedrovelmo@gmail.com>
2017-08-31 13:39:15 -0300

    primeiro commit epico

---
+++ HistoryOfGames/GameScene.swift
@@ -0,0 +1,35 @@
+//
+//  GameScene.swift
+//  HistoryOfGames
+//
+//  Created by Pedro Velmovitsky on 31/08/17.
+//  Copyright © 2017 adabestgroup. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+
+    override func didMove(to view: SKView) {
+
+ 
+           }
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+ 
+        
+    }
+
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
