4351804fd17a33f2e42c571e58c5112f235bad2b
grezende <grezende91@gmail.com>
2017-10-31 11:40:26 -0200

    Adjusted turtle speed

--- HistoryOfGames/Pattern.swift
+++ HistoryOfGames/Pattern.swift
@@ -92,22 +92,18 @@ class Pattern{
                 obstacle.texturesArray.append(SKTexture(imageNamed: textureName))
             }
             
-            
-            
             obstacle.run(SKAction.repeatForever(SKAction.animate(with: obstacle.texturesArray, timePerFrame: 0.06, resize: true, restore: false)))
             //obstacle.size = CGSize(width: 40, height: 40)
             obstacle.setScale(0.10)
-             obstacle.position.y = CGFloat(arc4random_uniform(UInt32(scene.size.height - obstacle.size.height / 2))) + floorPosition
+            obstacle.position.y = CGFloat(arc4random_uniform(UInt32(scene.size.height - obstacle.size.height / 2))) + floorPosition
             
             obstacle.position.x = scene.size.width + 20
-             let randomX = Int(arc4random_uniform(10)) + 5
-             let randomY = -Int(arc4random_uniform(10)) - 5
+             let randomX = Int(arc4random_uniform(5)) + 5
+             let randomY = -Int(arc4random_uniform(7)) - 5
             setObstaclePhysicsBody(obstacle: obstacle)
             obstacle.physicsBody?.applyImpulse(CGVector(dx: randomX, dy: randomY))
             obstacle.physicsBody?.restitution = 0.0
            // obstacle.physicsBody?.affectedByGravity = true
-            
-            
             
         case "block1":
 
