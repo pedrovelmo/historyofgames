0afd0438f17c0c04fe62fe62c02b0e32ed8f43ac
Pedro Velmovitsky <pedrovelmo@gmail.com>
2017-11-24 12:57:09 -0200

    Store View Controller added with Odessa

---
+++ HistoryOfGames/StoreCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  StoreCollectionViewCell.swift
+//  HistoryOfGames
+//
+//  Created by Pedro Velmovitsky on 24/11/17.
+//  Copyright © 2017 adabestgroup. All rights reserved.
+//
+
+import UIKit
+
+class StoreCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
---
+++ HistoryOfGames/StoreViewController.swift
@@ -0,0 +1,67 @@
+//
+//  StoreViewController.swift
+//  HistoryOfGames
+//
+//  Created by Pedro Velmovitsky on 24/11/17.
+//  Copyright © 2017 adabestgroup. All rights reserved.
+//
+
+import UIKit
+
+class StoreViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+
+    
+    @IBOutlet weak var menuB: UIButton!
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var textView: UITextView!
+    @IBAction func menuButton(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    var games = ["odessaRunframe1"]
+    var selectedCell: Int = 1
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
+        
+        //menuB.imageView?.image = UIImage(named: "menu")
+//        var color = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1.0)
+//        menuB.tintColor = color
+      //  menuB.layer.backgroundColor = .none
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return games.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "reuseIdentifier",
+                                                      for: indexPath) as! StoreCollectionViewCell
+        //2
+        cell.imageView.image = UIImage(named: games[indexPath.row])
+        selectedCell = indexPath.row
+        updateTextView()
+        return cell
+    }
+    
+    func updateTextView() {
+        
+        switch(selectedCell){
+        case 0:
+            textView.text = "Odessa is an awesome game developed by Apple Developer Academy Rio! Click here to see more information"
+        default:
+            textView.text = ""
+            
+        }
+        
+    }
+
+}
